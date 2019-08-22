5adab81b4af475c82d6a66dc3ca1bd4e0702f4a3
lucaskahl <lucaschangkahl@gmail.com>
2019-08-21 23:24:01 -0300

    Manipulado arquivo de forma sync e async

---
+++ 1-node-farm/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("FIle has been written!");
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", err => {
+        console.log("The file has been written 😍");
+      });
+    });
+  });
+});
