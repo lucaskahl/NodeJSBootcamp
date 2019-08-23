66a74d538fbfbec15c45f47da5c641c87815289e
lucaskahl <lucaschangkahl@gmail.com>
2019-08-23 06:39:32 -0300

    Criado uma simples API sem Express

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,11 +1,31 @@
 const fs = require("fs");
 const http = require("http");
+const path = require("path");
 
 // __________________________  S E R V E R  __________________________ //
 // ___________________________________________________________________ //
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  res.end("Hello world!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the Overview");
+  } else if (pathName === "/product") {
+    res.end("This is the products");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "internal-header": "ERR: 404"
+    });
+
+    res.end("Page not found - 404");
+  }
 });
 
 server.listen(3000, () => {
