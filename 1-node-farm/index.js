025077f986ec0023cefb36b0e46b50852b5b10d7
lucaskahl <lucaschangkahl@gmail.com>
2019-11-16 11:03:25 -0300

    Adicionado view Products e leitor de view

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -4,20 +4,51 @@ const path = require("path");
 
 // __________________________  S E R V E R  __________________________ //
 // ___________________________________________________________________ //
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+ 
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output
+}
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObject = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the Overview");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace(`{%PRODUCT_CARDS%}`, cardsHtml);
+
+    res.end(output);
+
+    // Product page
   } else if (pathName === "/product") {
     res.end("This is the products");
+
+    // Api
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+    
+    // Nou found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
